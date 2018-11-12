1087c10cb40775a91497322bd27d8ebf404bc3be
Zhen Wang(hazywang) <zhen.wang@twotalltotems.com>
2018-11-12 14:33:27 -0800

    use sarsa and q_learning to realize very simple rl examples

--- find_treasure.py
+++ find_treasure.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import time
 
+num_states = 6
+actions = ['left', 'right']
 epsilon = 0.9
 alpha = 0.1
 gamma = 0.9
@@ -13,12 +15,10 @@ refresh_interval = 0.3
 class Q_table(object):
 
     def __init__(self):
-        self.num_states = 6
-        self.actions = ['left', 'right']
         self.table = self._build_q_table()
 
     def _build_q_table(self):
-        table = pd.DataFrame(np.zeros(self.num_states, len(self.actions)), colums=self.actions)
+        table = pd.DataFrame(np.zeros((num_states, len(actions))), columns=actions,)
         return table
 
 
@@ -31,7 +31,7 @@ class Env(object):
 
     def get_env_feedback(self, S, A):
         if A =='right':
-            if S == self.num_states-2:
+            if S == num_states-2:
                 S1 = 'terminal'
                 reward = 1
             else:
@@ -46,41 +46,40 @@ class Env(object):
         return S1 , reward
 
     def update_env(self, S, episode, step_counter):
-        self.env_list = ['-'] *(self.num_states-1) + [ 'T']
+        self.env_list = ['-'] *(num_states-1) + [ 'T']
         if S == 'terminal':
-            print('\r{}'.format(episode+1), step_counter)
+            print('\r{}'.format("episode {}".format(episode+1)), "steps {}".format(step_counter))
             time.sleep(1)
         else:
             self.env_list[S] = 'o'
-            print(self.env_list)
+            print(''.join(self.env_list))
             time.sleep(1)
 
 
 class Agent(object):
     def __init__(self):
-        self.actions = ['left', 'right']
         self.state = 0
 
     def choose_action(self, q_table):
         state_actions = q_table.iloc[self.state, :]
         if (np.random.uniform(0,1) > epsilon) or ((state_actions == 0).all()):
-            action_name = np.random.choice(self.actions)
+            action_name = np.random.choice(actions)
         else:
             action_name = state_actions.idxmax()
         return action_name
 
 
-def run():
-
+def q_learning():
+    q_table = Q_table()
     for episode in range(max_episodes):
         agent = Agent()
-        q_table = Q_table()
+
         env = Env(episode=episode)
         step_counter = 0
         is_terminated = False
         while not is_terminated:
             A = agent.choose_action(q_table.table)
-            S_next, reward = Env.get_env_feedback(agent.state, A)
+            S_next, reward = env.get_env_feedback(agent.state, A)
             q_predict =q_table.table.loc[agent.state, A]
             if S_next != 'terminal':
                 q_target = reward + gamma * q_table.table.iloc[S_next, :].max()
@@ -92,9 +91,34 @@ def run():
             step_counter +=1
             env.update_env(agent.state, episode, step_counter)
 
+    print(q_table.table)
 
-    print(q_table)
 
+def sarsa():
+    q_table = Q_table()
+    for episode in range(max_episodes):
+        agent = Agent()
+        env = Env(episode=episode)
+        step_counter = 0
+        is_terminated = False
+        A = agent.choose_action(q_table.table)
+        while not is_terminated:
+            S = agent.state
+            S_next, reward = env.get_env_feedback(agent.state, A)
+            q_predict =q_table.table.loc[agent.state, A]
+            agent.state = S_next
+            if S_next != 'terminal':
+                A_next =  agent.choose_action(q_table.table)
+                q_target = reward + gamma * q_table.table.loc[S_next, A_next]
+            else:
+                q_target = reward
+                is_terminated = True
+            q_table.table.loc[S, A] += alpha * (q_target - q_predict)
+            A = A_next
+            step_counter +=1
+            env.update_env(agent.state, episode, step_counter)
+
+    print(q_table.table)
 
 if __name__ == '__main__':
-    run()
+    sarsa()
